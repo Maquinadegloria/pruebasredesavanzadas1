c2222874b7be887b48d459016ee0cc0c4f3c07ee
NetAcad DEVASC <changeme@example.com>
2024-06-27 22:32:21 +0000

    eliminamos la modularizacion de prueba 3

--- Lineas-de-codigo-prueba-3.py/campus.py
+++
@@ -1,6 +0,0 @@
-class Campus:
-    """Representa un campus con una descripción y una lista de dispositivos."""
-    def __init__(self, nombre, descripcion):
-        self.nombre = nombre  # Nombre del campus
-        self.descripcion = descripcion  # Descripción del campus
-        self.dispositivos = []  # Lista de dispositivos en el campus
--- Lineas-de-codigo-prueba-3.py/main.py
+++
@@ -1,6 +0,0 @@
-from administrador_redes import AdministradorRedes
-
-if __name__ == "__main__":
-    archivo_datos = "datos_red.json"
-    admin_redes = AdministradorRedes(archivo_datos)
-    admin_redes.menu_principal()
